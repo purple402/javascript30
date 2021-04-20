de35177db87009013f061fc9cc6372dd5749f7af
Joy Lee <purple402@naver.com>
2021-04-20 11:42:44 +0900

    make greenScreen filter

--- day19/scripts.js
+++ day19/scripts.js
@@ -31,6 +31,8 @@ function paintToCanvas() {
         // pixels = redEffect(pixels);
         
         pixels = rgbSpllit(pixels);
+        
+        pixels = greenScreen(pixels);
         // put them back
         ctx.putImageData(pixels, 0, 0)
     }, 16);
@@ -66,6 +68,33 @@ function rgbSpllit(pixels) {
         pixels.data[i - 150] = pixels.data[i + 2];// blue
     }
     return pixels;
+}
+
+function greenScreen(pixels) {
+    const levels = {};
+
+    document.querySelectorAll('.rgb input').forEach((input) => {
+      levels[input.name] = input.value;
+    });
+  
+    for (i = 0; i < pixels.data.length; i = i + 4) {
+      red = pixels.data[i + 0];
+      green = pixels.data[i + 1];
+      blue = pixels.data[i + 2];
+      alpha = pixels.data[i + 3];
+  
+      if (red >= levels.rmin
+        && green >= levels.gmin
+        && blue >= levels.bmin
+        && red <= levels.rmax
+        && green <= levels.gmax
+        && blue <= levels.bmax) {
+        // take it out!
+        pixels.data[i + 3] = 0;
+      }
+    }
+  
+    return pixels; 
 }
 
 
