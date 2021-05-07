eb9943d841c9373458ea26f5b01c860636b9ee87
Joy Lee <purple402@naver.com>
2021-05-07 14:58:25 +0900

    make displayTimeLeft function

--- day29/scripts.js
+++ day29/scripts.js
@@ -1,7 +1,9 @@
 let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
 function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
     countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
         // check if we should stop it!
@@ -9,5 +11,15 @@ function timer(seconds) {
             clearInterval(countdown);
             return;
         }
+        // display it
+        displayTimeLeft(secondsLeft);
     }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`
+    document.title = display;
+    timerDisplay.textContent = display;
 }
