ffffb0e720d5abc7d7498b8c2866ae42e89c0ccc
Joy Lee <purple402@naver.com>
2021-05-07 14:57:04 +0900

    make timer function

---
+++ day29/scripts.js
@@ -0,0 +1,13 @@
+let countdown;
+function timer(seconds) {
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+        // check if we should stop it!
+        if(secondsLeft < 0) {
+            clearInterval(countdown);
+            return;
+        }
+    }, 1000);
+}
